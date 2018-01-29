419e0a20927d9c89c21c0ddeb6644314369bad01
ardelain <delainarthur@gmx.fr>
2018-01-29 11:22:05 +0100

    structure des classes et premier formulaire json

--- serveur/routes/rechercheController.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var app = express();
-
-
-router.post('/rechercheAction', function rechercheAction (req, res) {
-    res.send('recherche !');
-});
