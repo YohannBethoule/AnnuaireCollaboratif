420849e12d94f670afa78a5611980f47dc7e546e
Yannis Perrot <yannis.perrot@etu.uca.fr>
2018-02-11 22:02:40 +0100

    databases.js

--- serveur/routes/database.js
+++ serveur/routes/database.js
@@ -5,7 +5,7 @@ function CreateUsersDataBase() {
     var connection = mysql.createConnection(dbconfig.users.connection);
 
     connection.query('CREATE DATABASE ' + dbconfig.users.database);
-
+/*
     connection.query('\
         CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.user_table + '` ( \
             `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
@@ -16,7 +16,7 @@ function CreateUsersDataBase() {
             UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
             UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
         )');
-
+*/
     console.log('Success: Database Created!')
     connection.end();
 }
@@ -29,7 +29,7 @@ function CreateDataBase() {
     //commentaire :
     connection.query('\
         CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.data.comment_table + '` ( \
-            `username` VARCHAR(20) NOT NULL, \
+            `username` VARCHAR(200) NOT NULL, \
             `domain_name` VARCHAR(200) NOT NULL, \
             `text` text NOT NULL, \
         )');
@@ -37,14 +37,68 @@ function CreateDataBase() {
     // mark [...]
     connection.query('\
         CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.data.Mark_table + '` ( \
-            `username` VARCHAR(20) NOT NULL, \
+            `username` VARCHAR(200) NOT NULL, \
+            `domain_name` VARCHAR(200) NOT NULL, \
+            `object` VARCHAR(50) NOT NULL, \
+            `note` int(11) NOT NUL, \
+        )');
+
+    // utilisateurs
+    connection.query('\
+        CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.data.User_table + '`(\
+        `username` VARCHAR(200) NOT NULL, \
+        `password` VARCHAR(200) NOT NULL, \
+        `mail` VARCHAR(200) NOT NULL, \
+        )');
+
+    // website
+    connection.query('\
+        CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.data.Website_table + '`(\
+        `domain_name` VARCHAR(200) NOT NULL, \
+        )');
+
+    // INDEXES
+    connection.query('\
+        ALTER TABLE `' + dbconfig.database + '`.`' + dbconfig.data.comment_table + '`(\
+        ADD KEY `fk_user_comment` \(`username`\)`, \
+        ADD KEY `fk_site_comment` \(`domain_name`\)`, \
+        )');
+
+    connection.query('\
+        ALTER TABLE `' + dbconfig.database + '`.`' + dbconfig.data.Mark_table + '`(\
+        ADD KEY `fk_user_mark` \(`username`\)`, \
+        ADD KEY `fk_site_mark` \(`domain_name`\)`, \
+        )');
+
+    connection.query('\
+        ALTER TABLE `' + dbconfig.database + '`.`' + dbconfig.data.User_table + '`(\
+        ADD PRIMARY KEY \(`username`\)`, \
+        ADD UNIQUE KEY `password` \(`password`, `mail`\)`, \
+        )');
+
+    connection.query('\
+        ALTER TABLE `' + dbconfig.database + '`.`' + dbconfig.data.Website_table + '`(\
+        ADD PRIMARY KEY \(`domain_name`\)`, \
+        )');
+
+    // CONSTRAINTS
+
+    connection.query('\
+        ALTER TABLE `' + dbconfig.database + '`.`' + dbconfig.data.comment_table + '`(\
+         ADD CONSTRAINT `fk_site_comment` FOREIGN KEY \(`domain_name`\) REFERENCES `Website` \(`domain_name`\), \
+         ADD CONSTRAINT `fk_user_comment` FOREIGN KEY \(`username`\) REFERENCES `User` \(`username`\), \
+        )');
+
+    connection.query('\
+        ALTER TABLE `' + dbconfig.database + '`.`' + dbconfig.data.Mark_table + '`(\
+         ADD CONSTRAINT `fk_site_mark` FOREIGN KEY \(`domain_name`\) REFERENCES `Website` \(`domain_name`\), \
+         ADD CONSTRAINT `fk_user_mark` FOREIGN KEY \(`username`\) REFERENCES `User` \(`username`\), \
         )');
 
     console.log('Success: Database Created!')
     connection.end();
 }
 
-//... mettre les autre table
 module.exports = {
     createUsersDataBase : CreateUsersDataBase
 } ;
