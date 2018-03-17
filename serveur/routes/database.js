5a31ac18f474ff3715a4934f593a0a1cd7b9e155
Yannis Perrot <yannis.perrot@etu.uca.fr>
2018-03-17 11:43:50 +0100

    ajout mld + description dans rapport

--- serveur/routes/database.js
+++ serveur/routes/database.js
@@ -18,7 +18,8 @@ CreateDataBase = function() {
             `email` VARCHAR(100) NOT NULL, \
             PRIMARY KEY (`id`), \
             UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
-            UNIQUE INDEX `username_UNIQUE` (`username` ASC)\
+            UNIQUE INDEX `username_UNIQUE` (`username` ASC),\
+            UNIQUE INDEX `mail_UNIQUE` (`email` ASC)\
     )');
 
     connection.query('\
